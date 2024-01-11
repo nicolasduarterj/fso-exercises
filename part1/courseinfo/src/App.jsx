0efc5a3fd47a74aecc877d26e58ca63156d9742d
nicolasduarterj <nicolas_duarterj@proton.me>
2024-01-10 23:07:33 -0300

    Exercises 1.3 -> 1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -5,29 +5,35 @@ import viteLogo from '/vite.svg'
 
 
 function App() {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2= 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14  
-
+  const course = {
+    name: 'Half stack application development',
+    parts: [ 
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+  
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name:'State of a component',
+        exercises: 14  
+      }
+    ]
+  }
+  
   return (
     <div>
-      <Header course={course}/>
-      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}/>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course.name}/>
+      <Content array={course.parts}/>
+      <p>Number of exercises: {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}</p>
     </div>
   )
 }
 
-/**
- * Função que gera o header
- *
- * @prop {string} course
- * @return {jsx} 
- */
+
 const Header = (props) => {
   return (
     <>
@@ -36,30 +42,18 @@ const Header = (props) => {
   )
 }
 
-/**
- * Componente que aglomera os headers das partes do curso
- * 
- * @prop {string} part (3 no total)
- * @prop {int} exnum (3 no total)
- * @returns {jsx}
- */
+
 const Content = (props) => {
   return (
     <>
-      <Part part={props.part1} exnum={props.exercises1}/>
-      <Part part={props.part2} exnum={props.exercises2}/>
-      <Part part={props.part3} exnum={props.exercises3}/>
+      <Part part={props.array[0].name} exnum={props.array[0].exercises} />
+      <Part part={props.array[1].name} exnum={props.array[1].exercises} />
+      <Part part={props.array[2].name} exnum={props.array[2].exercises} />
     </>
   )
 }
 
-/**
- *Componente que gera o h1 da parte
- *
- * @prop {string} part
- * @prop {int} exnum
- * @return {jsx} 
- */
+
 const Part = (props) => {
   return (
     <>
