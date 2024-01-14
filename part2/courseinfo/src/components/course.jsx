53fc9f4a044bffbe04d45ee2feece5512544ca3c
nicolasduarterj <nicolas_duarterj@proton.me>
2024-01-13 23:28:56 -0300

    Code reformatting
    Mainly shifted some components around and modified them to be more functional

--- part2/courseinfo/src/components/course.jsx
+++ part2/courseinfo/src/components/course.jsx
@@ -1,13 +1,15 @@
 const Course = ({name, parts}) => {
-    
-    const exercicios = parts.map((part) => part.exercises)
-    console.log(exercicios)
-    return(
-        <>
-        <Header course={name}></Header>
-        <Content array={parts}></Content>
-        <NumEx exArray={exercicios}/>
-        </>
+  
+  /** Isso acessa a array de objetos (parts) e mapeia cada indíce da array a exercícios
+   * a um número de exercícios de um objeto da array (parts)*/
+  const exercicios = parts.map((part) => part.exercises)
+  console.log(exercicios)
+  return(
+      <>
+      <Header course={name}/>
+      <Content array={parts}/>
+      <NumEx exArray={exercicios}/>
+      </>
     )
 }
 
@@ -19,23 +21,22 @@ const Header = ({course}) => {
     )
   }
   
-  const NumEx = ({exArray}) => <p>Number of exercises {exArray.reduce((accumulator, currentValue) => accumulator + currentValue)}</p>
+const Content = ({array}) => {
+    return (
+      <>
+      {array.map(parte => <Part title={parte.name} exnum={parte.exercises} key={parte.id}/>)}
+      </>
+    )
+  }
+  
+  
+const Part = ({title, exnum}) => <h1>{title} {exnum}</h1>
 
-  const Content = ({array}) => {
-    return (
-      <>
-        {array.map(parte => <Part title={parte.name} exnum={parte.exercises} key={parte.id}/>)}
-      </>
-    )
-  }
-  
-  
-  const Part = ({title, exnum}) => {
-    return (
-      <>
-        <h1>{title} {exnum}</h1>
-      </>
-    )
-  }
+
+const NumEx = ({exArray}) => <p>Number of exercises {somaArray(exArray)}</p>
+
+//Esse reduce soma todos os numeros na array recebida como parâmetro
+const somaArray = (arr) => arr.reduce((accumulator, currentValue) => accumulator + currentValue)
+
 
   export default Course
