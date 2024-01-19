e6ebe3594ebd3c0d374e65ad60f2dec5b7e29cc5
nicolasduarterj <nicolas_duarterj@proton.me>
2024-01-19 06:51:47 -0300

    Exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,13 +1,10 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import axios from 'axios'
+
 function App() {
-  const [persons, setPersons] = useState([
-    {name: "Arto Hellas", number:"2345-678"},
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState("") 
   const [newNum, setNewNum] = useState("")
 
@@ -38,6 +35,16 @@ function App() {
     console.log(event.target.value)
     setNewNum(event.target.value)
   }
+  
+  useEffect(() => {
+    console.log("Starting effect")
+    axios
+      .get('http://localhost:3001/persons').then(response => {
+        console.log("Promise fulfilled")
+        setPersons(response.data)
+      })
+  }, [])
+  
   return (
     <div> 
      <h2>Phonebook</h2>
