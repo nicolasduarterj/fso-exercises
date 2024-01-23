6bf3324c9c3595330087a75d97cf68b4b01d3b66
nicolasduarterj <nicolas_duarterj@proton.me>
2024-01-22 22:05:30 -0300

    Finish exercise 2.18

--- part2/dataforcountries/src/App.jsx
+++ part2/dataforcountries/src/App.jsx
@@ -6,18 +6,24 @@ import apiservices from "./apiservices"
 function App() {
   const [paises, setPaises] = useState([])
   const [filtro, setFiltro] = useState("")
-  const [specEnough, setspecEnough] = useState(false)
+  const [specEnough, setspecEnough] = useState(0)
   const [filtrados, setFiltrados] = useState([])
 
   const updateFiltro = event => {
     setFiltro(event.target.value)
     const filtroHolder = paises.filter(pais => pais.name.common.includes(event.target.value))
-    if (filtroHolder.length < 11) {
-      setspecEnough(true)
+    console.log(filtroHolder.length)
+    if (filtroHolder.length < 11 && filtroHolder.length > 1) {
+      setspecEnough(1)
       setFiltrados(filtroHolder)
     }
+    else if (filtroHolder.length < 2) {
+      setFiltrados(filtroHolder)
+      setspecEnough(2)
+    }
     else {
-      setspecEnough(false)
+      setFiltrados(filtroHolder)
+      setspecEnough(0)
     }
   }
 
@@ -38,7 +44,7 @@ function App() {
 }
 
 const Lista = ({arr, bool}) => {
-  if (bool) {
+  if (bool == 1) {
     return (
       <>
         <ul>
@@ -47,9 +53,36 @@ const Lista = ({arr, bool}) => {
       </>  
     )
   }
+  else if (bool == 2) {
+    return (
+      <>
+      <PaisCompleto pais={arr[0]}/>
+      </>
+    )
+  }
   return (
     <p>please be more specific</p>
   )
 }
+
+const PaisCompleto  = ({pais}) => {
+  const linguaprop = pais.languages
+  const properties = Object.keys(linguaprop)
+  const languages = properties.map(prope => linguaprop[`${prope}`])
+  return (
+    <>
+      <h2>{pais.name.common}</h2>
+      <p>capital {pais.capital}</p>
+      <p>area {pais.area}</p>
+      <p><b>languages:</b></p>
+      <ul>
+        {languages.map(lingua => <li key={lingua}>{lingua}</li>)}
+      </ul>
+      <img src={pais.flags.png}/>
+
+    </>
+  )
+}
+
 
 export default App
